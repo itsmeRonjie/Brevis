a8d1923174f760ae0210cf6db59c370e47cb6357
Ronjie Diafante Man-on <rdmanon@vauldex.com>
2025-04-23 09:27:48 +0800

    - project initiliation

---
+++ Brevis/BrevisApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrevisApp.swift
+//  Brevis
+//
+//  Created by Ronjie Diafante Man-on on 4/23/25.
+//
+
+import SwiftUI
+
+@main
+struct BrevisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
